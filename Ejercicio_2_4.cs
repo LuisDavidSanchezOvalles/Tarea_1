95ede3b2ea1a1ba9fd51085980207269daf7aef0
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-01-18 16:23:10 -0400

    Add files via upload

---
+++ Ejercicio_2_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Entrada = "";
+            int Celcius = 0; 
+            float Resultado = 0;
+
+            try
+            {
+                Console.Write("Ingresar Grados Centrigados a Covertir: ");
+                Entrada = Console.ReadLine();
+                Celcius = Convert.ToInt32(Entrada);
+
+                Resultado = (Celcius * 1.8f) + 32;
+
+                Console.WriteLine("{0} Grados Fahrenheit: ", Resultado);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Error...");
+            }
+        }
+    }
+}
